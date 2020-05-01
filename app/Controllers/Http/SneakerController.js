63fe4b210858eea71aa61299a6f26ab76990fc53
Ana Nikolova <ani.nik19@gmail.com>
2020-05-01 17:29:48 +0200

    read data && display all sneakers

--- app/Controllers/Http/SneakerController.js
+++ app/Controllers/Http/SneakerController.js
@@ -3,12 +3,21 @@ const Sneaker = use("App/Models/Sneaker");
 
 class SneakerController {
   async index({ view }) {
-    return view.render("sneakers/index");
+    let allSneakers = await Sneaker.all();
+    allSneakers = allSneakers.toJSON();
+    //console.log(allSneakers.toJSON());
+    let viewData = {
+      allSneakers,
+    };
+    console.log(viewData);
+    return view.render("sneakers/index", viewData);
   }
+
   async create({ view }) {
     return view.render("sneakers/create");
   }
-  async store({ request, view }) {
+
+  async store({ request, view, response }) {
     try {
       const sneaker = new Sneaker();
       const data = request.post();
@@ -18,7 +27,7 @@ class SneakerController {
       sneaker.description = data.description;
 
       await sneaker.save();
-      return "Saved to database";
+      response.redirect("/");
     } catch (error) {
       console.log(error);
       return "Sorry there was an error check console";
@@ -27,7 +36,7 @@ class SneakerController {
   async show({ view }) {
     return view.render("sneakers/show");
   }
-  async eidt({ view }) {
+  async edit({ view }) {
     return view.render("sneakers/edit");
   }
   async update({ view }) {
